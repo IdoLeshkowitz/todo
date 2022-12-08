9b089566e96dbead22904435d1515490c8fb1289
IdoLeshkowitz <idoleshkowitz@gmail.com>
2022-12-08 18:09:07 +0200

    node

--- todo/src/components/Main/TodoItem/TodoInput/TodoInputMode.tsx
+++ todo/src/components/Main/TodoItem/TodoInput/TodoInputMode.tsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
-import classNames from "classnames"
+import classNames from "classnames";
 const TodoInputMode = (props: any) => {
-	const { todoItem, editingMode, setEditingMode, updateTodo } = props;
+  const { todoItem, editingMode, setEditingMode, updateTodo } = props;
 
-	const onInputBlur = () => {
-		setEditingMode(false);
-	};
-	const onInputKeyDown = (event: any) => {
-		if (event.key === "Enter") {
-			console.log("Enter key pressed");
-			setEditingMode(false);
-			const updatedTodo = {
-				...todoItem,
-				title: event.target.value,
-			};
-			updateTodo(updatedTodo);
-		}
-	};
+  const onInputBlur = () => {
+    setEditingMode(false);
+  };
+  const onInputKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      console.log("Enter key pressed");
+      setEditingMode(false);
+      const updatedTodo = {
+        ...todoItem,
+        title: event.target.value,
+      };
+      updateTodo(updatedTodo);
+    }
+  };
 
-	const uneditedTodoTitle = todoItem.title;
-	return (
-		<li>
-			<input
-				className={classNames({
-					edit: editingMode,
-					"new-todo": true,
-				})}
-				type="text"
-				autoFocus
-				value={todoItem.text}
-				onBlur={onInputBlur}
-				onKeyDown={onInputKeyDown}
-			/>
-		</li>
-	);
+  const uneditedTodoTitle = todoItem.title;
+  return (
+    <li>
+      <input
+        className={classNames({
+          edit: editingMode,
+          "new-todo": true,
+        })}
+        type="text"
+        autoFocus
+        value={todoItem.text}
+        onBlur={onInputBlur}
+        onKeyDown={onInputKeyDown}
+      />
+    </li>
+  );
 };
 
 export default TodoInputMode;
